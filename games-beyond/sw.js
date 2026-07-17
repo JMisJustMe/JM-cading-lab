fe1f48249a7db0919c40081de19fb038b0778d77
JMisJustMe <jayemmlyrikz@gmail.com>
2026-07-17 05:22:27 +0100

    Cache Games&Beyond v0.2 mounted bodies for offline rooms

--- games-beyond/sw.js
+++ games-beyond/sw.js
@@ -1,5 +1,13 @@
-const CACHE='games-beyond-v0-1a';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./registry.json','./android-route.json'];
+const CACHE='games-beyond-v0-2-full-mount';
+const CORE=[
+  './','./index.html','./house-v0-2.js','./manifest.webmanifest','./icon.svg',
+  './registry.json','./bundled-bodies.json','./android-route.json',
+  './bodies/futarized-v1-2.html',
+  './bodies/fight-clash-v0-4.html',
+  './bodies/aiming-run-v0-1a.html',
+  './bodies/drag-aim-loop-kernel-v0-9-9.html',
+  './bodies/loopit-glyphplay-gameforge-boundary-rejoin-node.html'
+];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{

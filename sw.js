5ce7f0d1a71b05fd7c9ea683fe8984a638c0d215
JMisJustMe <jayemmlyrikz@gmail.com>
2026-07-17 05:41:28 +0100

    Add offline service worker for JM Web Estate

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='jm-web-estate-v1.0.0';
+const CORE=['./','./index.html','./estate.css','./estate-app.js','./manifest.webmanifest','./icon.svg','./registry/estate-map.json','./games-beyond/registry.json','./registry/theory-wing.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))));
+});

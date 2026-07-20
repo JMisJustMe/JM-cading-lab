86598bde9952599cca9419dc23e02049a0468038
JMisJustMe <jayemmlyrikz@gmail.com>
2026-07-20 17:27:37 +0100

    Cache author route in Estate v1.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jm-web-estate-v1.2.0';
-const CORE=['./','./index.html','./estate.css','./estate-accessibility.css','./estate-accessibility.js','./estate-app.js','./manifest.webmanifest','./icon.svg','./registry/estate-map.json','./registry/lyrics-house.json','./games-beyond/registry.json','./registry/theory-wing.json','./theory/','./lyrics/','./recovery/'];
+const CACHE='jm-web-estate-v1.3.0';
+const CORE=['./','./index.html','./estate.css','./estate-accessibility.css','./estate-accessibility.js','./estate-app.js','./manifest.webmanifest','./icon.svg','./registry/estate-map.json','./author/','./author/index.html','./author/author-public.json','./registry/lyrics-house.json','./games-beyond/registry.json','./registry/theory-wing.json','./theory/','./lyrics/','./recovery/'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
